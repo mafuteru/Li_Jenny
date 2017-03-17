8797982fd3a96219de1a248e23b7a117fd62799c
mafuteru <linkpichucheez@gmail.com>
2017-03-17 13:38:01 -0700

    14.2 again

--- APLesson_14/14.2/GMC.java
+++ APLesson_14/14.2/GMC.java
@@ -1,25 +1,9 @@
 public class GMC extends Car
-{
-	private double x, y;
-	
-	public GMC(double xC, double yC)
+{	
+	public GMC(double x, double y)
 	{
-		x=xC;
-		y=yC;
-	}
-	
-	public void move(double xC, double yC)
-	{
-		x+=xC;
-		y+=yC;
-	}
-	
-	public double[] getLoc()
-	{
-		double[] location= new double[2];
+		location= new double[2];
 		location[0]=x;
 		location[1]=y;
-
-		return location;
 	}
 }
