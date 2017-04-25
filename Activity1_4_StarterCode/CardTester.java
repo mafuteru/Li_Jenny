c604a016277b973bd31d3c7541b06bb78bd56d25
mafuteru <linkpichucheez@gmail.com>
2017-04-25 14:34:34 -0700

    11

--- Activity1_4_StarterCode/CardTester.java
+++ Activity1_4_StarterCode/CardTester.java
@@ -8,6 +8,34 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("hearts","K",13);
+		Card card2 = new Card("spades","K",13);
+		Card card3 = new Card("diamonds","10",10);
+		Card card4 = new Card("diamonds","10",10);
+		
+		System.out.println(card1.rank());
+		System.out.println(card2.rank());
+		System.out.println(card3.rank());
+		System.out.println();
+		
+		System.out.println(card1.suit());
+		System.out.println(card2.suit());
+		System.out.println(card3.suit());
+		System.out.println();
+		
+		System.out.println(card1.pointValue());
+		System.out.println(card2.pointValue());
+		System.out.println(card3.pointValue());
+		System.out.println();
+		
+		System.out.println(card1.toString());
+		System.out.println(card2.toString());
+		System.out.println(card3.toString());
+		System.out.println();
+		
+		System.out.println(card1.matches(card2));
+		System.out.println(card2.matches(card3));
+		System.out.println(card3.matches(card4));
+		
 	}
 }
